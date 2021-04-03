b4b25ff64502ec59e143edc01031bd42bab20f53
Muhtasim Ulfat <taaanmo0oy@gmail.com>
2021-04-03 06:27:39 +0600

    Channel scaffold add

--- Concept/channel/target/debug/deps/channel-9e44750e33b48bb1.d
+++
@@ -1,7 +0,0 @@
-/Users/tanmoy/memory/projects/Rust-Playground/Concept/channel/target/debug/deps/channel-9e44750e33b48bb1.rmeta: src/lib.rs
-
-/Users/tanmoy/memory/projects/Rust-Playground/Concept/channel/target/debug/deps/libchannel-9e44750e33b48bb1.rlib: src/lib.rs
-
-/Users/tanmoy/memory/projects/Rust-Playground/Concept/channel/target/debug/deps/channel-9e44750e33b48bb1.d: src/lib.rs
-
-src/lib.rs:
--- Concept/channel/target/debug/libchannel.d
+++
@@ -1,1 +0,0 @@
-/Users/tanmoy/memory/projects/Rust-Playground/Concept/channel/target/debug/libchannel.rlib: /Users/tanmoy/memory/projects/Rust-Playground/Concept/channel/src/lib.rs
