2abb1c0e2d4752755f389c5c10d679cc0bff5ee6
Muhtasim Ulfat <taaanmo0oy@gmail.com>
2021-04-03 06:25:31 +0600

    Scaffold add

--- Concept/channel/src/lib.rs
+++ Concept/channel/src/lib.rs
@@ -1,3 +1,33 @@
+use std::sync::{Arc, Condvar, Mutex};
+
+pub struct Sender<T> {
+    inner: Arc<Mutex<Inner<T>>>,
+}
+
+pub struct Receiver<T> {
+    inner: Arc<Mutex<Inner<T>>>,
+}
+
+pub struct Inner<T> {
+    queue: Vec<T>,
+}
+
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let inner = Inner {
+        queue: Vec::new()
+    };
+    let inner = Arc:: new(Mutex::new(inner));
+    (
+        Sender{
+            inner: inner.clone()
+        },
+        Receiver{
+            inner: inner.clone()
+        },
+    )
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ Concept/channel/target/debug/libchannel.d
@@ -0,0 +1,1 @@
+/Users/tanmoy/memory/projects/Rust-Playground/Concept/channel/target/debug/libchannel.rlib: /Users/tanmoy/memory/projects/Rust-Playground/Concept/channel/src/lib.rs
