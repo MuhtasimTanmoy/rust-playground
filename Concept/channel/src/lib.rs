4ffed19d6bde2ed511bfaaa82dea184d72fce5da
Muhtasim Ulfat <taaanmo0oy@gmail.com>
2021-04-04 08:04:24 +0600

    Condvar mutext usage on channel

--- Concept/channel/src/lib.rs
+++ Concept/channel/src/lib.rs
@@ -1,20 +1,47 @@
 use std::sync::{Arc, Condvar, Mutex};
+use std::collections::VecDeque;
 
+#[derive(Clone)]
 pub struct Sender<T> {
     inner: Arc<Inner<T>>,
 }
 
+impl <T> Sender<T> {
+    pub fn send(&mut self, t: T) {
+        let mut queue = self.inner.queue.lock().unwrap();
+        queue.push_back(t);
+        drop(queue);
+        self.inner.available.notify_one();
+    }
+}
+
 pub struct Receiver<T> {
-    inner: Arc<Inner<T>>,
+    inner: Arc<Inner<T>>
+}
+
+impl <T> Receiver<T> {
+    pub fn recv(&mut self) -> T {
+        let mut queue = self.inner.queue.lock().unwrap();
+        loop {
+            match queue.pop_front() {
+                Some(t) => return t,
+                None => {
+                    self.inner.available.wait(queue);
+                }
+            }
+        }
+    }
 }
 
 pub struct Inner<T> {
-    queue: Mutex<Vec<T>>,
+    queue: Mutex<VecDeque<T>>,
+    available: Condvar,
 }
  
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
     let inner = Inner {
-        queue: Mutex::default()
+        queue: Mutex::default(),
+        available: Condvar:: new(),
     };
     let inner = Arc:: new(inner);
     (
---
+++ Doc/Channel.md
@@ -0,0 +1,16 @@
+# Channel
+
+Data structures needed
+
+- Arc
+    - Reference counter to address same object queue in sender, receiver.
+
+- Condvar
+    - When it is waiting for it to be awakened it can wake up for unknown reasons by processor.
+    - Wait gives up the lock just before it goes to sleep.
+    - After wait is finished, the mutex is given back.
+     
+- Mutex
+    - Similar to binary sepaphore with kernel level optimization.
+    - Should be given on both sender receiver.
+ 
